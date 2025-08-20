b9c7a4fb1fb85d627862461b83f2a44bffb302ee
Saurabh Sharma <saurabh11sharma@live.com>
2025-08-21 00:22:37 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // <-- add this line for GitHub Pages
+  base: "./", // ✅ important for GitHub Pages
 });
